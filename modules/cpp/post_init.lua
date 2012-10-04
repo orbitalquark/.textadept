6a347b24fcf800da6e793a82c4042bd44e8639f0
mitchell <70453897+orbitalquark@users.noreply.github.com>
2012-10-04 16:24:02 -0400

    Added cpp/post_init.lua.

---
+++ modules/cpp/post_init.lua
@@ -0,0 +1,15 @@
+
+local e = _M.textadept.editing
+-- Indent on 'Enter' when between auto-paired '{}'.
+events.connect(events.CHAR_ADDED, function(ch)
+  if buffer:get_lexer() ~= 'cpp' or ch ~= 10 or not e.AUTOINDENT then return end
+  local buffer = buffer
+  local line = buffer:line_from_position(buffer.current_pos)
+  if buffer:get_line(line - 1):find('{%s+$') and
+     buffer:get_line(line):find('^%s*}') then
+    buffer:new_line()
+    buffer.line_indentation[line] = buffer.line_indentation[line - 1] +
+                                    buffer.tab_width
+    buffer:goto_pos(buffer.line_indent_position[line])
+  end
+end)
