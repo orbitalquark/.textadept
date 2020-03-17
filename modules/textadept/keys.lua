24d02ade4919ec2791c3aadb7927dfbfbb392033
mitchell <70453897+orbitalquark@users.noreply.github.com>
2020-03-17 17:18:37 -0400

    Updated key bindings based on recent changes.

--- modules/textadept/keys.lua
+++ modules/textadept/keys.lua
@@ -16,10 +16,10 @@ local keys, GUI = keys, not CURSES
 keys.cr = require('open_file_mode')
 -- TODO: io.open_recent_file
 -- TODO: io.reload_file
-keys.cw = io.save_file
--- TODO: io.save_file_as
+keys.cw = buffer.save
+-- TODO: buffer.save_as
 -- TODO: io.save_all_files
-keys.cx = io.close_buffer
+keys.cx = buffer.close
 -- TODO: io.close_all_buffers
 -- TODO: textadept.session.load
 -- TODO: textadept.session.save
@@ -284,6 +284,23 @@ keys.cl = buffer.vertical_centre_caret
 keys['c '] = function() buffer.selection_mode = buffer.SEL_STREAM end
 keys['c]'] = buffer.swap_main_anchor_caret
 
+-- Unbound keys are handled by Scintilla, but when playing back a macro, this is
+-- not possible. Define useful default key bindings so Scintilla does not have
+-- to handle them.
+keys.left, keys.sleft = buffer.char_left, buffer.char_left_extend
+keys.cleft, keys.csleft  = buffer.word_left, buffer.word_left_extend
+keys.right, keys.sright = buffer.char_right, buffer.char_right_extend
+keys.cright, keys.csright = buffer.word_right, buffer.word_right_extend
+keys.down, keys.sdown = buffer.line_down, buffer.line_down_extend
+keys.up, keys.sup = buffer.line_up, buffer.line_up_extend
+keys.home, keys.shome = buffer.vc_home, buffer.vc_home_extend
+keys['end'], keys.send = buffer.line_end, buffer.line_end_extend
+keys.del, keys.sdel = buffer.clear, buffer.cut
+keys.cdel = buffer.del_word_right
+keys.csdel = buffer.del_line_right
+keys['\b'], keys['s\b'] = buffer.delete_back, buffer.delete_back
+keys['c\b'], keys['cs\b'] = buffer.del_word_left, buffer.del_line_left
+
 -- Miscellaneous not in standard menu.
 keys[GUI and 'aW' or 'mW'] = function()
   buffer:drop_selection_n(buffer.selections - 1)
@@ -330,15 +347,16 @@ for key, v in pairs{[next_key] = true, [prev_key] = false} do
     for i = 1, #_VIEWS do
       local buffer_type = _VIEWS[i].buffer._type
       if buffer_type == _L['[Files Found Buffer]'] then
-        ui.find.goto_file_found(false, v)
+        ui.find.goto_file_found(nil, v)
         if view == _VIEWS[i] then ui.goto_view(orig_view) end -- nothing found
         return
       elseif buffer_type == _L['[Message Buffer]'] then
-        textadept.run.goto_error(false, v)
+        textadept.run.goto_error(nil, v)
         if view == _VIEWS[i] then ui.goto_view(orig_view) end -- nothing found
         return
       end
     end
+    ui.find[v and 'find_next' or 'find_prev']()
   end
 end
 keys[GUI and 'a<' or 'm<'] = function() buffer:line_scroll(-20, 0) end
@@ -355,7 +373,7 @@ events.connect(events.LEXER_LOADED, function(lang)
     keys[lang]['\t'] = function()
       if buffer:auto_c_active() then return end -- ignore
       return buffer.selection_empty and
-             (textadept.snippets._insert() == nil or
+             (textadept.snippets.insert() == nil or
               textadept.editing.autocomplete(lang) == true or
               textadept.editing.autocomplete('lsp') == true)
     end
@@ -420,8 +438,8 @@ ekeys.cf = function() ui.command_entry:char_right() end
 ekeys[GUI and 'af' or 'mf'] = function() ui.command_entry:word_right() end
 ekeys.cb = function() ui.command_entry:char_left() end
 ekeys[GUI and 'ab' or 'mb'] = function() ui.command_entry:word_left() end
-ekeys.cn = function() ui.command_entry:line_down() end
-ekeys.cp = function() ui.command_entry:line_up() end
+ekeys.cn = ekeys.down -- cycle history next
+ekeys.cp = ekeys.up -- cycle history prev
 ekeys.ca = function() ui.command_entry:vc_home() end
 ekeys.ce = function() ui.command_entry:line_end() end
 ekeys.cv = function() ui.command_entry:page_down() end
